f3c10ac7c5820a14af0712de240e45b0c3c171a0
Nazir Hajje <nhajje@hajje.com>
2018-10-15 23:18:37 +0300

    use router and parameters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { GitSearchService } from './git-search.service';
-import { GitSearchUserService } from './git-search-user.service';
 
 @Component({
   selector: 'app-root',
@@ -8,22 +7,11 @@ import { GitSearchUserService } from './git-search-user.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
-  constructor(private GitSearchService: GitSearchService, private GitSearchUserService: GitSearchUserService){
+  constructor(private GitSearchService: GitSearchService){
 
   }
-  ngOnInit(){
-    this.GitSearchService.gitSearch('beginer').then((response) => {
-      alert("Total Libraries Found:" + response.total_count );
-    }, (error) => {
-      alert("Error: " + error.statusText)
-    })
-    this.GitSearchUserService.gitSearchUser('nazir').then( (response) =>{
-      alert("Total Users Found: " + response.total_count );
-    }, (error) => {
-      alert("Error: " + error.statusText)
-    })
-  }
+  ngOnInit(){  }
   
   title = 'GitHub Browser';
-  name = 'Nazir Hajje';
+  
 }
